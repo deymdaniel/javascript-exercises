9e35bbf78d3bfc5a3c6062fd6ec360dab3dc0cc5
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-16 00:55:30 +0800

    Finished reverse string test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let newString = "";
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
 };
+
+reverseString("daniel");
 
 // Do not edit below this line
 module.exports = reverseString;
