d3b3092b8f701400ca6528cb1f97c58a7777f06b
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-15 19:20:22 +0800

    Finished repeat string test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+ let newString = ""
+ if (num < 0){
+   return "ERROR"
+ }
+ for (i = 0; i<num; i++){
+    newString = newString + str;
+    
+ }//console.log(newString) 
+ return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
