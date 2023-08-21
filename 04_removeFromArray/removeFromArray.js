cfe43e495152fc3421876930e5c605476c198e57
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-21 21:03:46 +0800

    Finished remove from array test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  const newArr = arr.filter((element) => !args.includes(element));
+  return newArr;
 };
+removeFromArray([1, 2, 3, 4, 5, 6], 2, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
