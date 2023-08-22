71e7d81411c09ca30bdccea55aaa65c2994fb955
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-22 12:20:13 +0800

    Finished sum all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  if (
+    first < 0 ||
+    second < 0 ||
+    Number(first) !== first ||
+    Number(second) !== second
+  ) {
+    return "ERROR";
+  } else if (first > second) {
+    return ((first - second + 1) * (second + first)) / 2;
+  } else if (second > first) {
+    return ((second - first + 1) * (first + second)) / 2;
+  }
 };
+sumAll(5, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
