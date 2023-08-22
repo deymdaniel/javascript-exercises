18fbe8e5e85d5c33ee6e857e03cc225c530fb179
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-22 13:16:33 +0800

    Finished temp conversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let temp = (f - 32) * (5 / 9);
+  if (temp % 1 != 0) {
+    return Math.round(temp * 1e1) / 1e1;
+  } else return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let temp = c * (9 / 5) + 32;
+  if (temp % 1 != 0) {
+    return Math.round(temp * 1e1) / 1e1;
+  } else return temp;
 };
 
+convertToCelsius(72);
+convertToFahrenheit(100);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
