868d3ffdc28b041e0e7d047aca2a71e3be2db9bd
Daniel Biacan III <danielbiacaniii@gmail.com>
2023-08-22 12:53:58 +0800

    Finished leap years test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 == 0) {
+    if (year % 400 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year % 4 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+leapYears(1900);
 // Do not edit below this line
 module.exports = leapYears;
